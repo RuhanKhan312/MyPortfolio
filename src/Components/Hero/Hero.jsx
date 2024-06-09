dbdefc13ab24485729528e2b538431329ac20046
RuhanKhan312 <khanruhan312@gmail.com>
2024-06-09 11:21:45 +0530

    modified hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { FaFacebook } from "react-icons/fa";
 
 const navigation = [
   { name: 'Home', href: '#' },
@@ -112,15 +113,7 @@ export default function Example() {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
+          
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Hi, I am Ruhan
@@ -129,15 +122,19 @@ export default function Example() {
               I am a Front-end/Full-Stack Software developer. I am currently learning and working with
               MERN, and Flutter as well. I am looking to collaborate on software projects.
             </p>
+            <div className="flex justify-center items-center w-1/2">
+                <FaFacebook />
+            </div>
+            
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Download Resume
               </a>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
+                Contact me <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
